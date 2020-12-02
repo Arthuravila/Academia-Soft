6f273ea8d058139b183907412bfaf043452dde77
unknown <aavila.bio@gmail.com>
2020-12-01 22:18:45 -0300

    Adjust toolbar

--- app/src/main/java/com/app/academiasoft/MainActivity.kt
+++ app/src/main/java/com/app/academiasoft/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.app.academiasoft
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
+    }
+
 }
--- app/src/main/java/com/app/academiasoft/ui/home/HomeFragment.kt
+++ app/src/main/java/com/app/academiasoft/ui/home/HomeFragment.kt
@@ -34,7 +34,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     private fun setClickListeners() {
         binding.buttonAluno.setOnClickListener {
-            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment(""))
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment("Aluno"))
         }
 
         binding.buttonInstrutor.setOnClickListener {
